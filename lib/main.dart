13ecb4b03b24b6f522b517d7d2175da0ebca58ac
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-30 01:51:00 +0430

    new project added

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ShopApp(),
+    );
+  }
+}
+
+class ShopApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shop'),
+      ),
+      body: Center(
+        child: Text('shop page center'),
+      ),
+    );
+  }
+}
