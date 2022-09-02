5a69dfa8fc6dc9daf7da43cadf785c32536fc3ef
shadmehr <shadmehrmanoochehri@gmial.com>
2022-09-02 23:30:12 +0430

    add condition to return item is it favorite item or not

--- lib/widgets/gridViewBuilder.dart
+++ lib/widgets/gridViewBuilder.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import '../provider/product_provider.dart';
 import '../widgets/product_items.dart';
+
 // ignore: use_key_in_widget_constructors
 class GridViewProduct extends StatelessWidget {
+  final bool isFavorite;
+  // ignore: use_key_in_widget_constructors, prefer_const_constructors_in_immutables
+  GridViewProduct(this.isFavorite);
+
   @override
   Widget build(BuildContext context) {
-    final productData = Provider.of<ProductProvider>(context).items;
+    final products = Provider.of<ProductProvider>(context);
+    final productData = isFavorite ? products.favoriteItems : products.items;
     return GridView.builder(
         padding: const EdgeInsets.all(10.0),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -15,14 +22,13 @@ class GridViewProduct extends StatelessWidget {
             crossAxisSpacing: 15,
             mainAxisSpacing: 20),
         itemBuilder: (context, index) => ChangeNotifierProvider.value(
-            value: productData[index], 
-            child: ProductItems(
-                // productData[index].id,
-                // productData[index].title,
-                // productData[index].imageUrl,
-                ),
-          ),
-            itemCount: productData.length
-        );
+              value: productData[index],
+              child: ProductItems(
+                  // productData[index].id,
+                  // productData[index].title,
+                  // productData[index].imageUrl,
+                  ),
+            ),
+        itemCount: productData.length);
   }
 }
