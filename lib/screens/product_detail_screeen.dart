e4b5cec78edcafdc34882bd6152513aac46f249e
shadmehr <shadmehrmanoochehri@gmial.com>
2022-09-08 13:33:24 +0430

    add detail of product

--- lib/screens/product_detail_screeen.dart
+++ lib/screens/product_detail_screeen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../provider/product_provider.dart';
@@ -8,10 +10,35 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)?.settings.arguments as String;
-    final productLoaded =
-        Provider.of<ProductProvider>(context , listen: false).findById(productId);
+    final productLoaded = Provider.of<ProductProvider>(context, listen: false)
+        .findById(productId);
     return Scaffold(
       appBar: AppBar(title: Text(productLoaded.title)),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 10.0),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image.network(
+                  productLoaded.imagUrl,
+                  fit: BoxFit.cover,
+                ),
+                Text(
+                  productLoaded.title,
+                  style: TextStyle(
+                    backgroundColor: Theme.of(context).accentColor,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              productLoaded.description,
+            ),
+            Text('Price : \$ ${productLoaded.price}')
+          ],
+        ),
+      ),
     );
   }
 }
