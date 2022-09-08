295aa218fc04d70c594a5982d6d69b2ffd03ecc9
shadmehr <shadmehrmanoochehri@gmial.com>
2022-09-08 13:36:17 +0430

    add dismissible to delete orders

---
+++ lib/widgets/cart_items.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../provider/cart.dart';
+import 'package:provider/provider.dart';
+
+class CartItem extends StatelessWidget {
+  final String id;
+  final String productId;
+  final double price;
+  final int quantity;
+  final String title;
+
+  // ignore: use_key_in_widget_constructors, prefer_const_constructors_in_immutables
+  CartItem(
+      {required this.id,
+      required this.productId,
+      required this.price,
+      required this.title,
+      required this.quantity});
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20.0),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItems(productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListTile(
+            leading: CircleAvatar(
+                child: FittedBox(
+              child: Text('\$$price'),
+            )),
+            title: Text(title),
+            subtitle: Text('total : (${price * quantity})'),
+            trailing: Text('$quantity X'),
+          ),
+        ),
+      ),
+    );
+  }
+}
