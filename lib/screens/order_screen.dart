35c4694cae4c1d9a7422896bcbf81f1ace3612d4
shadmehr <shadmehrmanoochehri@gmial.com>
2022-10-09 22:59:19 +0330

    tranfer data to show in order screen

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -4,21 +4,52 @@ import '../provider/order.dart' as ord;
 import '../widgets/order_items.dart';
 import '../widgets/drawer.dart' as d;
 
-class Order extends StatelessWidget {
+class Order extends StatefulWidget {
   static const routeName = '/oreder';
+
+  @override
+  State<Order> createState() => _OrderState();
+}
+
+class _OrderState extends State<Order> {
+  var _isLoading = false;
+  var _isInit = true;
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<ord.Order>(context, listen: false)
+          .fetchAndStoreOrders()
+          .then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<ord.Order>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Order list'),
+        title: const Text('Order list'),
       ),
       drawer: d.AppDrawer(),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: ((context, index) => OrderItem(orderData.orders[index])),
-      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: ((context, index) =>
+                  OrderItem(orderData.orders[index])),
+            ),
     );
   }
 }
